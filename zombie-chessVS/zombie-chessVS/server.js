5c126493dfc6838b7c65a2d8e4db6eb8accba50e
eden0102lee <eden0102lee@gmail.com>
2026-01-14 18:51:49 +0800

    Align test UI with stable version

--- zombie-chessVS/zombie-chessVS/server.js
+++ zombie-chessVS/zombie-chessVS/server.js
@@ -32,6 +32,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/test", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "test", "index.html"));
+});
+
 // 另外增加一個專門的 health check API，有些平台會找這個
 app.get("/health", (req, res) => {
     res.status(200).send("OK");
